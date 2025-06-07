fba31054a8bf45273b7fb8640eff281bac880022
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-06-07 19:55:32 +0700

    Environment Refactor

--- Assets/Scripts/Game Object/Board.cs
+++ Assets/Scripts/Game Object/Board.cs
@@ -5,10 +5,10 @@ public class Board : MonoBehaviour
     void OnMouseDown()
     {
         // Kiểm tra trạng thái của game trước khi thực hiện hành động
-        // if (GameController.Instance.IsGameRunning)
-        // {
-        //     Debug.Log("Touched Board");
-        //     GameController.Instance.EndGame("Touch The Board");
-        // }
+        if (GameController.Instance.isGameRunning)
+        {
+            Debug.Log("Touched Board");
+            GameController.Instance.GameOver("Touch The Board");
+        }
     }
 }
--- Assets/Scripts/Game Object/LightDecor.cs
+++ Assets/Scripts/Game Object/LightDecor.cs
@@ -5,11 +5,9 @@ public class LightDecor : MonoBehaviour
 {
     public static LightDecor Instance { get; private set; }
     private SpriteRenderer spriteRenderer;
-
-    // Màu mặc định và màu khi nhấn vào tile
-    private Color defaultColor = new Color(1f, 1f, 1f, 0.588f);  // Alpha = 150/255
-    private Color activeColor = new Color(1f, 1f, 1f, 1f);  // Alpha = 256/255
-    private Coroutine blinkCoroutine; // Coroutine để điều khiển chớp sáng
+    private Color defaultColor = new Color(1f, 1f, 1f, 0.588f);  
+    private Color activeColor = new Color(1f, 1f, 1f, 1f); 
+    private Coroutine blinkCoroutine; 
 
     void Awake()
     {
@@ -30,38 +28,26 @@ public class LightDecor : MonoBehaviour
 
         if (spriteRenderer != null)
         {
-            spriteRenderer.color = defaultColor; // Đặt màu mặc định cho sprite renderer
+            spriteRenderer.color = defaultColor; 
         }
     }
 
-    // Hàm thay đổi màu của LightDecor khi nhấn vào tile (và chớp sáng)
     public void ChangeLightContrast()
     {
         if (blinkCoroutine != null)
         {
-            StopCoroutine(blinkCoroutine); // Nếu coroutine đang chạy, dừng lại trước
+            StopCoroutine(blinkCoroutine); 
         }
 
-        blinkCoroutine = StartCoroutine(BlinkLight()); // Bắt đầu hiệu ứng chớp sáng
+        blinkCoroutine = StartCoroutine(BlinkLight()); 
     }
 
-    // Coroutine để tạo hiệu ứng chớp sáng
     private IEnumerator BlinkLight()
     {
         if (spriteRenderer == null) yield break;
-
-        // Đặt màu active
         spriteRenderer.color = activeColor;
-
-        // Chờ 0.2 giây (hoặc bất kỳ khoảng thời gian nào bạn muốn)
         yield return new WaitForSeconds(0.2f);
-
-        // Trả về màu mặc định
         spriteRenderer.color = defaultColor;
-
-        // Chờ thêm trước khi chớp tiếp (nếu cần lặp lại)
         yield return new WaitForSeconds(0.2f);
-
-        // Nếu muốn chớp nhiều lần, bạn có thể lặp logic này thêm tại đây
     }
 }
